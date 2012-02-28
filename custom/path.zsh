78964e6168e58f110869f5ba159818c45cb3c753
Chris Hunt <huntca@gmail.com>
2012-02-27 19:52:07 -0800

    Add custom dir to repo

---
+++ custom/path.zsh
@@ -0,0 +1,9 @@
+# PATH for MySQL
+export DYLD_LIBRARY_PATH=/usr/local/mysql/lib
+export PATH=/usr/local/mysql/bin:$PATH
+
+# PATH for MongoDB
+export PATH=/Applications/mongodb-osx-x86_64-2.0.0/bin:$PATH
+
+# Set /usr/local/bin before /usr/bin for Homebrew
+export PATH=/usr/local/bin:$PATH
