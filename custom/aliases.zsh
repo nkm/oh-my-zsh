54f9e669c11f1d59f6f33fb192f7c5e3deee9c0c
Chris Hunt <huntca@gmail.com>
2012-02-27 19:52:07 -0800

    Add custom dir to repo

---
+++ custom/aliases.zsh
@@ -0,0 +1,26 @@
+alias grep='ack'
+alias dus='du -hs'
+alias df='df -h'
+alias ls='ls -h'
+alias ll='ls -l'
+alias la='ls -a'
+alias l='ls -CF'
+alias lt='ls -ltr'
+alias vim='mvim -v'
+alias vi=vim
+alias dunnet='emacs -batch -l dunnet'
+alias tmux="TERM=screen-256color-bce tmux"
+alias md5sum='md5 -r'
+alias be='bundle exec'
+alias rspec='rspec --color'
+alias spec=rspec
+
+################
+# Climate.com
+################
+
+alias wb='cd /Users/huntca/workspace/weatherbill'
+alias tw='cd /Users/huntca/workspace/twain'
+alias vpn='sudo openconnect --script /usr/local/etc/vpnc/vpnc-script vpn.climate.com'
+alias wbgo='/Users/huntca/workspace/weatherbill/tools/wbgo'
+alias openoffice="/Applications/OpenOffice.org.app/Contents/MacOS/soffice.bin -headless -nofirststartwizard -accept='socket,host=localhost,port=8100;urp;StarOffice.Service'"
