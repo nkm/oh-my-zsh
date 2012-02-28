da3ecd27af45b3ca2402d3e010b6c1651a044350
Chris Hunt <c@squareup.com>
2012-02-27 20:39:10 -0800

    Remove Climate aliases

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -10,17 +10,6 @@ alias vim='mvim -v'
 alias vi=vim
 alias dunnet='emacs -batch -l dunnet'
 alias tmux="TERM=screen-256color-bce tmux"
-alias md5sum='md5 -r'
 alias be='bundle exec'
 alias rspec='rspec --color'
 alias spec=rspec
-
-################
-# Climate.com
-################
-
-alias wb='cd /Users/huntca/workspace/weatherbill'
-alias tw='cd /Users/huntca/workspace/twain'
-alias vpn='sudo openconnect --script /usr/local/etc/vpnc/vpnc-script vpn.climate.com'
-alias wbgo='/Users/huntca/workspace/weatherbill/tools/wbgo'
-alias openoffice="/Applications/OpenOffice.org.app/Contents/MacOS/soffice.bin -headless -nofirststartwizard -accept='socket,host=localhost,port=8100;urp;StarOffice.Service'"
